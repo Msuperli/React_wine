14f9700c213e51e0c49bf824f33ffea22d5f9c76
Msuperli <1693321385@qq.com>
2018-04-13 21:30:43 +0800

    react 1.0

---
+++ final/api/src/db/mysql.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+
+var db = mysql.createPool({
+    connectionLimit: 10,
+    host: "10.3.136.20",//ip地址
+    user: 'root',
+    port:3306,
+    password: '',
+    database: 'react_kuli'//数据库名字
+})
+
+
+module.exports = {
+    select: function(_sql, _callback){
+        db.query(_sql, function(error, results,fields){
+        //    console.log(results);
+            if(error){
+                _callback({status: false, error: error})
+            }else{
+               _callback({status: true, data: {results}});
+            }
+        })
+    }
+}
